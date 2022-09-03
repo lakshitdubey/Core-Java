5973d50ced86e734f145892b69bf37ae1dc0044e
Lakshit Dubey <lakshitd@gmail.com>
2022-09-03 15:06:29 +0530

    new

--- oops/polymorphism/directmethod/TestShape.java
+++ oops/polymorphism/directmethod/TestShape.java
@@ -6,15 +6,19 @@ public static void main(String[] args) {
 	
 	Circle c = (Circle) Shape.getShape(1);
 	c.setRadius(5);
-	
+	c.area();
 	
 	Rectangle r = (Rectangle) Shape.getShape(2);
 	r.setLength(12);
 	r.setWidth(6);
+	r.area();
+
+	
 	
 	Triangle t = (Triangle) Shape.getShape(3);
 	t.setBase(10);
 	t.setHeight(6);
+	t.area();
 	
 }
 }
